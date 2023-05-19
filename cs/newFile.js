811287f059b2a44d9d0594e87efb0cfb18da73cf
Lyonel Pierce <lyonel@live.com>
2023-05-19 15:03:52 -0400

    Added runtimes

--- cs/newFile.js
+++ cs/newFile.js
@@ -13,12 +13,23 @@ function addToZero(numbers) {
   return false;
 }
 
-console.log("AddToZero");
-console.log(addToZero([]));
-console.log(addToZero([1]));
-console.log(addToZero([1, 2, 3]));
-console.log(addToZero([1, 2, 3, -2]));
+console.time("addToZero []");
+console.log(addToZero([])); // 5.068ms
+console.timeEnd("addToZero []");
 
+console.time("addToZero [1]");
+console.log(addToZero([1])); // 0.069ms
+console.timeEnd("addToZero [1]");
+
+console.time("addToZero [1, 2, 3]");
+console.log(addToZero([1, 2, 3])); // 0.056ms
+console.timeEnd("addToZero [1, 2, 3]");
+
+console.time("addToZero [1, 2, 3, -2]");
+console.log(addToZero([1, 2, 3, -2])); // 0.056ms
+console.timeEnd("addToZero [1, 2, 3, -2]");
+
+console.log("----------------------------------------");
 // Unique Characters
 
 function hasUniqueChars(word) {
@@ -34,9 +45,14 @@ function hasUniqueChars(word) {
   return true;
 }
 
-console.log("UniqueChars");
-console.log(hasUniqueChars("Monday")); // O(n) => O(6) => O(1)
-console.log(hasUniqueChars("Moonday")); // O(n) => O(7) => O(1)
+console.time("hasUniqueChars 'Monday'");
+console.log(hasUniqueChars("Monday")); // 0.117ms // O(n) => O(6) => O(1)
+console.timeEnd("hasUniqueChars 'Monday'");
+
+console.time("hasUniqueChars 'Moonday'");
+console.log(hasUniqueChars("Moonday")); // 0.136ms // O(n) => O(7) => O(1)
+console.timeEnd("hasUniqueChars 'Moonday'");
+console.log("----------------------------------------");
 
 // Pangram Sentence
 
@@ -53,9 +69,14 @@ function isPangram(sentence) {
   return true;
 }
 
-console.log("isPangram");
-console.log(isPangram("The quick brown fox jumps over the lazy dog!"));
-console.log(isPangram("I like cats, but not mice"));
+console.time("isPangram 'The quick brown fox jumps over the lazy dog!'");
+console.log(isPangram("The quick brown fox jumps over the lazy dog!")); // 0.173ms
+console.timeEnd("isPangram 'The quick brown fox jumps over the lazy dog!'");
+
+console.time("isPangram 'I like cats, but not mice'");
+console.log(isPangram("I like cats, but not mice")); // 0.264ms
+console.timeEnd("isPangram 'I like cats, but not mice'");
+console.log("----------------------------------------");
 
 // Longest Word
 
@@ -71,5 +92,6 @@ function findLongestWord(words) {
   return longestLength;
 }
 
-console.log("findLongestWord");
-console.log(findLongestWord(["hi", "hello"]));
+console.time("findLongestWord");
+console.log(findLongestWord(["hi", "hello"])); // 0.19ms
+console.timeEnd("findLongestWord");
